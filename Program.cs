c62d91722a71e9093dc67f9a059e0eaab981da56
jirlinge <julienirlinger@gmail.com>
2021-02-18 09:15:44 +0100

    version corrigé

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result;
+            int nb1;
+            Console.WriteLine("choisissez un nombre");
+            result = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("voulez vous additionner ce nombre ?");
+            
+            string answer = Console.ReadLine();
+
+            while (answer!="non")
+            {
+                
+                Console.WriteLine("choisissez un nombre à additionner ?");
+
+                nb1 = Convert.ToInt32(Console.ReadLine());
+                result =  nb1 + result;
+                Console.WriteLine(result);
+
+                Console.WriteLine("continuer?");
+                answer = Console.ReadLine();
+                
+
+
+            }
+
+            Console.WriteLine("comme vous voulez");
+          
+        }
+    }
+}
